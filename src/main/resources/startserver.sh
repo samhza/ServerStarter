bce56d55ce13bbacb5e330ad168b13e39e7a3c49
Samuel Hernandez <sam@samhza.com>
2021-10-17 18:28:31 -0400

    cleanup startserver.sh, make it executable
    startserver.sh's execute bit was unset so trying to run the script would
    yield a "permission denied" error. Added a shebang at the top of the
    file so it will always run under the right shell. Also cleaned up a bit
    of the code including quoting some variables to avoid some globbing
    weirdness.

--- src/main/resources/startserver.sh
+++ src/main/resources/startserver.sh
@@ -1,31 +1,30 @@
+#!/usr/bin/env bash
 DO_RAMDISK=0
-if [[ $(cat server-setup-config.yaml | grep 'ramDisk:' | awk 'BEGIN {FS=":"}{print $2}') =~ "yes" ]]; then
-    SAVE_DIR=$(cat server.properties | grep 'level-name' | awk 'BEGIN {FS="="}{print $2}')
-    mv $SAVE_DIR "${SAVE_DIR}_backup"
-    mkdir $SAVE_DIR
-    sudo mount -t tmpfs -o size=2G tmpfs $SAVE_DIR
+if [[ $(grep 'ramDisk:' server-setup-config.yaml | awk 'BEGIN {FS=":"}{print $2}') =~ "yes" ]]; then
+    SAVE_DIR=$(server.properties grep 'level-name' | awk 'BEGIN {FS="="}{print $2}')
+    mv "$SAVE_DIR" "${SAVE_DIR}_backup"
+    mkdir "$SAVE_DIR"
+    sudo mount -t tmpfs -o size=2G tmpfs "$SAVE_DIR"
     DO_RAMDISK=1
 fi
 	if [ -f serverstarter-@@serverstarter-libVersion@@.jar ]; then
 			echo "Skipping download. Using existing serverstarter-@@serverstarter-libVersion@@.jar"
          java -jar serverstarter-@@serverstarter-libVersion@@.jar
                if [[ $DO_RAMDISK -eq 1 ]]; then
-               sudo umount $SAVE_DIR
-               rm -rf $SAVE_DIR
-               mv "${SAVE_DIR}_backup" $SAVE_DIR
+               sudo umount "$SAVE_DIR"
+               rm -rf "$SAVE_DIR"
+               mv "${SAVE_DIR}_backup" "$SAVE_DIR"
                fi
                exit 0
 	else
 			export URL="https://github.com/TeamAOF/ServerStarter/releases/download/v@@serverstarter-libVersion@@/serverstarter-@@serverstarter-libVersion@@.jar"
 	fi
 		echo $URL
-		which wget >> /dev/null
-		if [ $? -eq 0 ]; then
+		if command -v wget>/dev/null 2>&1; then
 			echo "DEBUG: (wget) Downloading ${URL}"
 			wget -O serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
    else
-			which curl >> /dev/null
-			if [ $? -eq 0 ]; then
+			if command -v curl>/dev/null 2>&1; then
 				echo "DEBUG: (curl) Downloading ${URL}"
 				curl -o serverstarter-@@serverstarter-libVersion@@.jar "${URL}"
 			else
@@ -34,7 +33,7 @@ fi
       fi
 java -jar serverstarter-@@serverstarter-libVersion@@.jar
 if [[ $DO_RAMDISK -eq 1 ]]; then
-    sudo umount $SAVE_DIR
-    rm -rf $SAVE_DIR
-    mv "${SAVE_DIR}_backup" $SAVE_DIR
+    sudo umount "$SAVE_DIR"
+    rm -rf "$SAVE_DIR"
+    mv "${SAVE_DIR}_backup" "$SAVE_DIR"
 fi
